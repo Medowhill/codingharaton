777f8820d6167ac1daffaf391f44804f588eb43f
Jaemin Hong <hjm0901@gmail.com>
2018-04-28 00:40:03 +0900

    change resource names

--- app/src/main/java/com/haraton/salad/codingharaton/activities/VoiceActivity.java
+++ app/src/main/java/com/haraton/salad/codingharaton/activities/VoiceActivity.java
@@ -1,12 +1,10 @@
 package
  com.haraton.salad.codingharaton.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -25,11 +23,11 @@ public class VoiceActivity extends AppCompatActivity {
             public void onClick(View view) {
                 ctrlBtn.setSelected(!ctrlBtn.isPressed());
                 if (ctrlBtn.isPressed()) { //clicked -> default
-                    ctrlBtn.setImageResource(R.drawable.voiceStartButton);
+                    ctrlBtn.setImageResource(R.drawable.voice_start_button);
                     instrTxt.setText(R.string.instruction_default);
                 }
                 else {//default -> clicked
-                    ctrlBtn.setImageResource(R.drawable.voiceStopButton);
+                    ctrlBtn.setImageResource(R.drawable.voice_stop_button);
                     instrTxt.setText(R.string.instruction_clicked);
                 }
 
